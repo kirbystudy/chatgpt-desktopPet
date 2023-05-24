7f42c89f73a08300ee0b746af4db67e42b2a4980
kirbystudy <783341822@qq.com>
2023-05-24 13:25:59 +0800

    改进代码样式

--- src/renderer/utils/chatting.js
+++ src/renderer/utils/chatting.js
@@ -153,7 +153,7 @@ function getReply(str) {
     formData.append('ruleType', config.gpt.ruleType);
     formData.append('command', str);
 
-    fetch(config.gpt.url, {
+    fetch(`${config.gpt.url}`, {
         method: 'POST',
         body: formData
     })
